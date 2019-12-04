60675cffd013f777d9f498a999c40dc62b3b8a2d
Cronus <seer20582609@gmail.com>
2019-12-04 10:48:10 +0800

    init layout design

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("test");
+window.onload = function () {
+    var block1 = document.createElement("div");
+    var block2 = document.createElement("div");
+    document.body.append(block1);
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("test");
+window.onload = () => {
+    var block1 = document.createElement("div");
+    var block2 = document.createElement("div");
+    document.body.append(block1);
+}
